200f108f91c2ecb915a7c66b185cd0040885dae8
Cavaari <cavaarit@gmail.com>
2023-12-16 19:01:43 -0500

    updated upload

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    let theme = localStorage.getItem('theme');
+    if(theme === null) {
+        const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+        localStorage.setItem('theme', (isDark ? 'dark' : 'light'));
+        theme = localStorage.getItem('theme');
+    }
+    let isDarkMode = (theme === 'dark' ? true : false);
+
+    // Initialize toggle button text and body class
+    $("#themeToggle")[0].innerHTML = (isDarkMode ? '☀️' : '🌚');
+    if(isDarkMode) {
+        $("body").addClass("dark");
+    } else {
+        $("body").addClass("light");
+    }
+
+    // Toggle dark mode event listener
+    $("#themeToggle").on("click", function() {
+        if(isDarkMode) {
+            localStorage.setItem('theme', 'light');
+            isDarkMode = false;
+            $("body").removeClass("dark").addClass("light");
+        } else {
+            localStorage.setItem('theme', 'dark');
+            isDarkMode = true;
+            $("body").removeClass("light").addClass("dark");
+        }
+
+        $("#themeToggle")[0].innerHTML = (isDarkMode ? '☀️' : '🌚');
+    });
+});
